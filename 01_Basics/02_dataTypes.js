28cdb10acb3b3ec474adaa2b97c57dceb4bf825a
Muhammad Abu Bakar bin Waseem <afhr4321@gmail.com>
2025-04-09 12:32:36 +0500

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all JS code as newer versionS
+
+//alert(3+3); // we are currently using nodeJS,not he browser
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("MAB")
+
+
+let name = "MAB"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
